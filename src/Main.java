d59bfc43ac1d087121edfad348dc24f007ab74bd
Сергей <100863938+Sergei190@users.noreply.github.com>
2023-03-27 19:17:36 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, ParseException {
         Scanner scanner = new Scanner(System.in);
-        String s; //
+        String s; 
         Basket shoppingCart;
         int selectedItem;
         int itemCount;
